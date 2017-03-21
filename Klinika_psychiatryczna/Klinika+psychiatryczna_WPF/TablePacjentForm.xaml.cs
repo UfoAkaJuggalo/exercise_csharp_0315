b645af0ecbbee9d8becb0f597c5cb72a7734ced7
Michal Nawrocki <v25@vault25.net>
2017-03-21 16:38:37 +0100

    First step to WPF transition

---
+++ Klinika_psychiatryczna/Klinika+psychiatryczna_WPF/TablePacjentForm.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Klinika_psychiatryczna.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Klinika_psychiatryczna_WPF
+{
+    /// <summary>
+    /// Interaction logic for TablePacjentForm.xaml
+    /// </summary>
+    public partial class TablePacjentForm : Window
+    {
+        private FormViewModel viewModel = new FormViewModel();
+
+        public TablePacjentForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Klinika_psychiatryczna/Klinika+psychiatryczna_WPF/ViewModels/FormViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Klinika_psychiatryczna.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klinika_psychiatryczna.ViewModels
+{
+    public class FormViewModel
+    {
+        public FormViewModel()
+        {
+            Pacjenci = new List<Pacjent>();
+        }
+        public List<Pacjent> Pacjenci { get; }
+    }
+}
